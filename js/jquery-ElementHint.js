a5e0353935e114aadcfe89f33f3db201651c18f6
Phil Corbett <corbett.phil@gmail.com>
2012-06-02 16:41:24 -0400

    Built a simple example page, add displaying hints to the js, add example xml for demos.

--- js/jquery-ElementHint.js
+++ js/jquery-ElementHint.js
@@ -1,22 +1,65 @@
 $(function() {
-	ElementHint.init();
-	ElementHint.EditMode(true);
+	ElementHint.Init()
+	//ElementHint.EditMode(true)
 });
 
 var ElementHint = (function($) {
+	
+	// Grabs the xml from a the file that contains all of the Hint META data. //Default: ElementHint.xml
 	var getHints = function () {
 		$.ajax({
-			url: 'path/to/xml.xml',
+			url: 'ElementHint.xml',
 			success: function (d) {
-				//woot
+				$(d).find('hint').each(function (i, e) {
+					_addHintToElement(_parseHint(e))
+				})
 			}
-		});
-	};
+		})
+	}
+	
+	// Add a Hint's HTML to the DOM for display includes events for that hint.
+	var _addHintToElement = function (inHint) {
+		$ElementGettingHint = $('#' + inHint.elementID)
+		
+		$ElementGettingHint.before(_buildHint(inHint))
+		
+		
+		// Create Events
+		$ElementGettingHint.on('hover.ElementHint', function () {
+			$('#' + inHint.hintID).show(300, 'swing')
+		})
+		
+		$('#' + inHint.hintID + '_Close').on('click.ElementHint', function() {
+			$('#' + inHint.hintID).hide(300, 'swing')
+		})
+	}
+	
+	// Parses the XML to create a Hint object.
+	var _parseHint = function (inHintXML) {
+		return {
+			title: $(inHintXML).find('title').text(),
+			body: $(inHintXML).find('body').text(),
+			elementID: $(inHintXML).find('elementID').text(),
+			hintID: 'ElementHint' + $(inHintXML).find('elementID').text()
+		}
+	}
+
+	// Creates the HTML that is used to display a Hint on the page.
+	var _buildHint = function (inHint) {
+		$hint = $('<div id="' + inHint.hintID + '" style="position: absolute; background-color: #FFFFFF; border: 1px solid #333333; display: none;"></div>')
+		$hint.append('<div>' + inHint.title + '</div>')
+		$hint.append('<div>' + inHint.body + '</div>')
+		$hint.append('<div><a href="javascript:void(0);" id="' + inHint.hintID + '_Close">Close</a></div>')
+		return $hint
+	}
 	
 	return {
-		init: function() {
+		// Initiates the use of the ElementHint functionality. Sets defaults for the plugin. 
+		Init: function() {
 			getHints()
 		},
+		
+		// Turns on/off the EditMode of ElementHint, allowing a user to create new hits from the page they're viewing.
 		EditMode: function(TurnOn){
 			if(TurnOn) {
 				$('[id]')
@@ -25,11 +68,10 @@ var ElementHint = (function($) {
 					.each(function(i, e) {
 						var $Editor = $('<div>Editor Design</div>')
 							.data('HintElementID', $(e).attr('id'))
-							
 					})
 			} else {
 				//do nothing or handle non-ID based elements.
 			}
 		}
 	}
-})(jQuery);
+})(jQuery)
